2824501d846a6df5dadf4588ce711f2f7e61a0b1
Rahul Raj <er.rahulraj@yahoo.com>
2026-05-02 15:05:03 +0530

    Add files via upload

--- SVR00066/src/main.tsx
+++ SVR00066/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import './styles/index.css';
 import { App } from './app/App';
+import { ErrorBoundary } from './app/components/ErrorBoundary';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </StrictMode>
 );
